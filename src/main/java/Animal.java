392d0b72f8588561b12ee9600ff43eb970097ab3
mmohchin18 <mohima.mohchin@gmail.com>
2021-09-30 10:09:09 +0100

    Comment animal

--- src/main/java/Animal.java
+++ src/main/java/Animal.java
@@ -1,34 +1,34 @@
-import java.time.LocalDate;
-
-public abstract class Animal {
-    private java.lang.Object System;
-
-    ////////////////Attributes///////////////////////////////////
-
-    private String breed;
-    private LocalDate dateOfBirth;
-    private String name;
-
-
-
-    //////////////////Constructors//////////////////////////////
-
-
-
-    //////////////////Methods/////////////////////////////////
-    public abstract String breathe();
-
-
-    public void sleep(){
-
-    }
-
-//    public void die(){
+//import java.time.LocalDate;
 //
-//        System.out.println("I am dead");
+//public abstract class Animal {
+//    private java.lang.Object System;
+//
+//    ////////////////Attributes///////////////////////////////////
+//
+//    private String breed;
+//    private LocalDate dateOfBirth;
+//    private String name;
+//
+//
+//
+//    //////////////////Constructors//////////////////////////////
+//
+//
+//
+//    //////////////////Methods/////////////////////////////////
+//    public abstract String breathe();
+//
+//
+//    public void sleep(){
+//
 //    }
-
-    public abstract String reproduce();
-
-    // public abstract java.lang.Object getClass();
-}
+//
+////    public void die(){
+////
+////        System.out.println("I am dead");
+////    }
+//
+//    public abstract String reproduce();
+//
+//    // public abstract java.lang.Object getClass();
+//}
